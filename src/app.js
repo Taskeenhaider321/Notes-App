f394062a88a080c263941b248e3fe3885468d951
Taskeen Haider <taskeenhaider509@gmail.com>
2022-09-27 17:03:31 -0700

    Completed Note console app

--- src/app.js
+++ src/app.js
@@ -1,57 +1,104 @@
 const express = require('express');
-const path = require('path');
-const hbs = require('hbs')
 const app = express();
 require('./db/connectdb')
 const Note = require('./model/addNotes');
-const url = require('url'); // built-in utility
 
 const port = process.env.PORT || 3000;
 
-const static_path = path.join(__dirname, '../public');
-const views_path = path.join(__dirname, '../templates/views');
-const partials_path = path.join(__dirname, '../templates/partials');
-
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(express.static(static_path))
-app.set('view engine', 'hbs')
-app.set('views', views_path)
-hbs.registerPartials(partials_path)
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
-app.get('/show', async (req, res) => {
+// Show Data From MongooDB
+app.get('/', async (req, res) => {
     try {
         const showNote = await Note.find({})
-        res.send(showNote)
-        var html = "";
-        show.forEach((element) => {
-          html += `
-              <div class="notes">
-                  <textarea cols="72" rows="7"  id="note-text"> ${element.text}</textarea>
-              </div>
-                  `
-            console.log(element.text);
-        })
-        document.getElementById("notes").innerHTML = html;
+        res.send(showNote)     
     } catch(e) {
         res.status(500).send(e);
     }
 })
-app.post('/add', async(req, res) => {
+
+// Add Data into MongooDB
+app.post('/add', async (req, res) => {
+    const addNotes = new Note(req.body);
     try {
-            const addingNotes = new Note ({
-                Notes : req.body.note,
-                Numbers : req.body.number,
-            })
-                const added = await addingNotes.save();
-                res.status(201).redirect('/')            
-    } catch (error) {
-        res.status(401).send(error)
-    }       
+        await addNotes.save()
+        res.status(201).send(addNotes)
+    } catch(e) {
+        res.status(400).send(e)
+    }
 })
+
+// Show Data in ascending form
+app.get('/ascend', async (req, res) => {
+    try {
+        const ascendNotes = await Note.find({}).sort({
+            Numbers : 1
+        })
+        res.status(201).send(ascendNotes);
+    } catch (e) {
+        res.status(400).send(e)  
+    }
+    
+})
+
+// Show Data in descending form
+app.get('/descend', async (req, res) => {
+    try {
+        const descendNotes = await Note.find({}).sort({
+            Numbers : -1
+        })
+        res.status(201).send(descendNotes);
+    } catch (e) {
+        res.status(400).send(e)  
+    }
+    
+})
+
+// Delete Data by Id
+app.delete('/notes/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const deleteNote = await Note.findByIdAndDelete(_id)
+        if (!deleteNote) {
+            res.status(404).send()
+        }
+        res.send(deleteNote)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// Delete ALl Data
+app.delete('/delete/all', async (req, res) => {
+    try {
+        const showNote = await Note.remove({})
+        res.send(showNote)  
+    } catch (e) {
+        res.status(500).send() 
+    }
+})
+
+// Update Data By Id
+app.patch('/update/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['Notes', 'Numbers']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+    const _id = req.params.id
+    try {
+        const updateNote = await Note.findByIdAndUpdate(_id, req.body, {new: true, runValidators: true})
+        if (!updateNote) {
+            res.status(404).send()
+        }
+        res.send(updateNote)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
 app.listen(port, () => {
     console.log(`This is the ${port} active port `);
 });
--- src/model/addNotes.js
+++ src/model/addNotes.js
@@ -1,21 +1,24 @@
 const mongoose = require('mongoose');
 const validator = require('validator')
-
 const notesSchema = new mongoose.Schema({
     Notes : {
         type: String,
         required: true,
-        validate(value) {
-            if (!validator.isAlpha(value)) {
-              throw new Error("Email is Not Valid!");
-            }
-          },
+        // validate(value) {
+        //     if (!validator.isAlpha(value)) {
+        //       throw new Error("Please Enter Only Notes!");
+        //     }
+        //   },
     },
     Numbers : {
         type: Number,
-        required: true
+        required: true,
+        validate(value) {
+          if (value > 15) {
+            throw new Error("Please Enter less than 15")
+          }
+        }
     }
 })
-
 const Note = new mongoose.model('Note',notesSchema)
 module.exports = Note
